95acd1a58c30d2f63995c79c7ab36444300de4e2
CokeU17 <cokeu17_lote@hotmail.com>
2026-08-09 12:07:46 -0600

    Implementacion del registro de clientes

--- SistemaBancario/SistemaBancario/SistemaBancario.cpp
+++ SistemaBancario/SistemaBancario/SistemaBancario.cpp
@@ -2,24 +2,40 @@
  * @file SistemaBancario.cpp
  * @brief Sistema de gestion de turnos para una sucursal bancaria.
  * @author Angel Teran
- * @version 0.2
+ * @version 0.3
  */
 
 #include <iostream>
+#include <string>
+#include <queue>
 
 using namespace std;
 
 /**
+ * @brief Clase que representa a un cliente del banco.
+ */
+class Cliente
+{
+public:
+    string nombre;
+    string tramite;
+    int turno;
+};
+
+/**
  * @brief Funcion principal del programa.
  *
- * Muestra el menu principal del sistema y permite al usuario
- * seleccionar las diferentes opciones disponibles.
+ * Permite registrar clientes y administrar el menu principal
+ * del sistema bancario.
  *
  * @return 0 si el programa finaliza correctamente.
  */
 int main()
 {
     int opcion;
+    int siguienteTurno = 1;
+
+    queue<Cliente> clientesEspera;
 
     do
     {
@@ -43,8 +59,30 @@ int main()
         switch (opcion)
         {
         case 1:
-            cout << "Opcion seleccionada: Registrar cliente." << endl;
+        {
+            Cliente nuevoCliente;
+
+            cin.ignore();
+
+            cout << "Nombre del cliente: ";
+            getline(cin, nuevoCliente.nombre);
+
+            cout << "Tipo de tramite: ";
+            getline(cin, nuevoCliente.tramite);
+
+            nuevoCliente.turno = siguienteTurno;
+
+            siguienteTurno++;
+
+            clientesEspera.push(nuevoCliente);
+
+            cout << endl;
+            cout << "Cliente registrado correctamente." << endl;
+            cout << "Turno asignado: "
+                << nuevoCliente.turno << endl;
+
             break;
+        }
 
         case 2:
             cout << "Opcion seleccionada: Ver siguiente cliente." << endl;
